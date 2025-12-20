0a093b5e672a52a04b5e89c62bd333dc6a2a1bfa
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 22:27:39 +0000

    Edo: auth.php

--- config/auth.php
+++ config/auth.php
@@ -26,3 +26,10 @@ return [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
+            'throttle' => 60,
+        ],
+    ],
+
+    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
+
+];
