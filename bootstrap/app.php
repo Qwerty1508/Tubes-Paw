2481459b75165a9418e68c2deaf746eef87c360a
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 23:00:41 +0000

    Edo: app.php botstrap

---
+++ bootstrap/app.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
