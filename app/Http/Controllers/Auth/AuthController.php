168e73f2e3d063c0837f84b128a269268e96faad
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 23:27:00 +0000

    Edo: app-Http-Controllers-Auth-AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,3 +12,9 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
