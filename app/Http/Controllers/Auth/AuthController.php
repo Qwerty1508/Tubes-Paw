71fafbeb7684ef7870046bf098e0426dba5a61ab
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 23:26:08 +0000

    Edo: app-Http-Controllers-Auth-AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,3 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
