00234b861d8248fedfa0e67e3b05fcf2f36ea02e
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 22:20:51 +0000

    Edo: database.php

--- config/database.php
+++ config/database.php
@@ -117,3 +117,10 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
+
+        'cache' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
