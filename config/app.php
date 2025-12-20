9d63b0e4f7175751c7bc02b53cb15dfead1daa91
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 22:09:41 +0000

    Edo: app.php

--- config/app.php
+++ config/app.php
@@ -6,3 +6,8 @@ return [
 
     'env' => env('APP_ENV', 'production'),
     
+    'debug' => (bool) env('APP_DEBUG', false),
+
+    'url' => env('APP_URL', 'http://localhost'),
+
+    'timezone' => 'UTC',
