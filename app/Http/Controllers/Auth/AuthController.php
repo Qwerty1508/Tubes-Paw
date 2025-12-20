ea5b2b786fe434d3360b9dcd64364759f29d9adf
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 23:37:19 +0000

    Edo: app-Http-Controllers-Auth-AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -68,3 +68,8 @@ class AuthController extends Controller
 
         Auth::logout();
         $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
