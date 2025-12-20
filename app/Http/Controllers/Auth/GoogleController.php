1420c465cd76e6b40960b73f97f01efb5282c742
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 23:39:19 +0000

    Edo: app-Http-Controllers-Auth-GoogleController.php

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -5,3 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
