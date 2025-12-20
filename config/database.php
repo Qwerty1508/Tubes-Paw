2ed9a38fb549d610ec95b900420a563f3ca0ad92
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 22:18:35 +0000

    Edo: database.php

--- config/database.php
+++ config/database.php
@@ -96,3 +96,10 @@ return [
     ],
 
     'redis' => [
+        
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'persistent' => env('REDIS_PERSISTENT', false),
