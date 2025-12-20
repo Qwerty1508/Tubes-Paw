d195ac7630fd1ff6b41a0a70ba21de4d52a57c49
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-20 22:13:32 +0000

    Edo: database.php

--- config/database.php
+++ config/database.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Str;
 return [
 
     'default' => env('DB_CONNECTION', 'sqlite'),
+    
+    'connections' => [
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
