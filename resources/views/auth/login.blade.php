113f796db2fdda243f186abf6298a854a540a40d
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-21 10:44:57 +0000

    Edo: resources-views-auth-login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,3 +61,10 @@
                                         <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
                                     </svg>
                                         <span data-i18n="login_with_google">{{ __('messages.login_with_google') }}</span>
+                                    </a>
+                                
+                                <div class="text-center my-4 position-relative">
+                                    <hr class="my-0">
+                                    <span class="position-absolute top-50 start-50 translate-middle bg-white px-3 text-muted small" data-i18n="login_with_email">
+                                        {{ __('messages.login_with_email') }}
+                                    </span>
