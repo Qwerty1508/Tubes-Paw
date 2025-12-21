79cea63132eff2a76a737bc34a7dc16f7ae3ec8d
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-21 00:08:26 +0000

    Edo: app-Http-Controllers-Auth-guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -96,3 +96,10 @@
         @yield('content')
     </main>
     
+    @include('components.footer')
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    
+    <script>
