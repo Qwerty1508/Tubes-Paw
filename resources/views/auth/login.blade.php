fa19140dbf2184d28817c6acc56dc22f3bb76464
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-21 10:50:04 +0000

    Edo: resources-views-auth-login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -159,3 +159,10 @@
 @push('scripts')
 <script>
     const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+    
+    if (togglePassword) {
+        togglePassword.addEventListener('click', function() {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            
