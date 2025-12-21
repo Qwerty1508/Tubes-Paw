512c974d7823ddb80b5f09a69f7e893bca16af31
Allfridho Saragi <pedoprimasaragi@gmail.com>
2025-12-21 11:03:14 +0000

    Edo: resources-views-auth-register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,3 +5,10 @@
 @section('content')
 <section class="min-vh-100 d-flex align-items-center bg-cream py-5">
     <div class="container">
+        <div class="row g-0 justify-content-center">
+            <div class="col-lg-10">
+                <div class="card shadow-xl overflow-hidden" style="border-radius: 1.5rem;">
+                    <div class="row g-0">
+                        <div class="col-lg-6 order-lg-1 order-2">
+                            <div class="p-5">
+                                <div class="text-center mb-4">
