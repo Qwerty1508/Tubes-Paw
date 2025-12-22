f8d012d177406bdb006207ed8af04c82ca38e4e0
haiidarmirza8289-alt <haiidarmirza8289@gmail.com>
2025-12-22 11:39:40 +0000

    Haidar: membuat routes

---
+++ resources/views/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <h2>Selamat Datang di Dashboard Admin Culinaire!</h2>
+        <p>Kelola menu, order, dan reservasi di sini.</p>
+    </div>
+</div>
+@endsection
